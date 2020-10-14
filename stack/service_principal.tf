362e0cf1fd7a850e7ed15ab90287c3cf89492138
Arnaud Rebts <arnaud.rebts@gmail.com>
2020-10-14 13:24:25 +0200

    feat: add contributor to aks's SP

--- stack/service_principal.tf
+++ stack/service_principal.tf
@@ -1,9 +1,16 @@
+data "azurerm_subscription" "current" {}
+
+data "azurerm_role_definition" "contributor" {
+  name = "Contributor"
+}
+
 resource "azuread_application" "k8s_cluster" {
   name = "${var.cluster_name}-name"
 }
 
 resource "azuread_service_principal" "k8s_cluster" {
   application_id = azuread_application.k8s_cluster.application_id
+  app_role_assignment_required = true
 }
 
 resource "azuread_service_principal_password" "k8s_cluster" {
@@ -15,4 +22,10 @@ resource "azuread_service_principal_password" "k8s_cluster" {
 resource "random_string" "password" {
   length  = 32
   special = true
+}
+
+resource "azurerm_role_assignment" "k8s_contributor" {
+  scope                = data.azurerm_subscription.current.id
+  role_definition_name = data.azurerm_role_definition.contributor.id
+  principal_id         = azuread_service_principal.k8s_cluster.id
 }
