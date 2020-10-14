85ceb975d93a8949628d61baa58635e5aa467746
Arnaud Rebts <arnaud.rebts@gmail.com>
2020-10-14 14:25:28 +0200

    fix: incorrect permission (#6)

--- stack/service_principal.tf
+++ stack/service_principal.tf
@@ -50,7 +50,6 @@ resource "azurerm_role_definition" "aks_sp_role_rg" {
       "Microsoft.Network/routeTables/routes/read",
       "Microsoft.Network/routeTables/routes/write",
       "Microsoft.Network/routeTables/routes/delete",
-      "Microsoft.Storage/storageAccounts/fileServices/fileShare/read",
       "Microsoft.ContainerRegistry/registries/read",
       "Microsoft.Network/publicIPAddresses/read",
       "Microsoft.Network/publicIPAddresses/write",
