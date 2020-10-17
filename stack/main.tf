bd230a0785dcbdd64058e6554de6afc3e3357431
Arnaud Rebts <arnaud.rebts@gmail.com>
2020-10-17 10:55:46 +0200

    feat: use RBAC for key vault

--- stack/main.tf
+++ stack/main.tf
@@ -38,39 +38,18 @@ resource "azurerm_kubernetes_cluster" "cluster" {
 }
 
 resource "azurerm_key_vault" "main" {
-  name                        = "kidibox"
-  sku_name                    = "standard"
-  location                    = azurerm_resource_group.k8s.location
-  resource_group_name         = azurerm_resource_group.k8s.name
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  enabled_for_disk_encryption = true
-  purge_protection_enabled    = false
-  soft_delete_enabled         = true
-  soft_delete_retention_days  = 7
-
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
-
-    key_permissions = [
-      "create",
-      "get",
-      "list",
-      "decrypt",
-      "encrypt",
-      "sign",
-      "verify",
-    ]
-
-    secret_permissions = [
-      "backup",
-      "get",
-      "list",
-      "recover",
-      "restore",
-      "set",
-    ]
-  }
+  name                            = "kidibox"
+  sku_name                        = "standard"
+  location                        = azurerm_resource_group.k8s.location
+  resource_group_name             = azurerm_resource_group.k8s.name
+  tenant_id                       = data.azurerm_client_config.current.tenant_id
+  enabled_for_deployment          = true
+  enabled_for_disk_encryption     = true
+  enabled_for_template_deployment = true
+  enable_rbac_authorization       = true
+  purge_protection_enabled        = false
+  soft_delete_enabled             = true
+  soft_delete_retention_days      = 7
 }
 
 resource "azurerm_key_vault_key" "sops" {
