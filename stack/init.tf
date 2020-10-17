4effe5358d2c217171145145fca60de053cb13be
Arnaud Rebts <arnaud.rebts@gmail.com>
2020-10-17 10:13:29 +0200

    feat: add azure key vault (#11)
    * feat: add azure key vault
    
    * chore: cleanup files

--- stack/init.tf
+++ stack/init.tf
@@ -15,7 +15,11 @@ provider "random" {}
 provider "azurerm" {
   # version = "=2.13.0"
   # version = ">=2.31.1"
-  features {}
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = true
+    }
+  }
 }
 
 provider "azuread" {
